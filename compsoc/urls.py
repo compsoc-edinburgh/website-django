c49be89d4177f64d5990b2ccdce5c90dfaf1a28c
Rikki Guy <rikki.guy@gmail.com>
2014-01-08 18:04:19 +0000

    Ability to search

--- compsoc/urls.py
+++ compsoc/urls.py
@@ -5,4 +5,5 @@ from compsoc import views
 urlpatterns = patterns('',
     url(r'^$', RedirectView.as_view(url='/compsoc/page/home'), name='go-to-home'),
     url(r'^page/(?P<page_name>\w+)', views.PageView.as_view(), name='page'),
+    url(r'^search', views.SearchView.as_view(), name='search'),
 )
--- compsoc/views.py
+++ compsoc/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import TemplateView
 
 from compsoc.models import Page, Event
+from compsoc.forms import SearchForm
 
 class PageView(TemplateView):
     template_name = 'compsoc/page.html'
@@ -13,3 +14,21 @@ class PageView(TemplateView):
         return render(request, self.template_name,  context)
     
 
+class SearchView(TemplateView):
+    template_name = 'compsoc/search.html'
+    
+    def get(self, request):
+        form = SearchForm(request.GET)
+        
+        if form.is_valid():
+        
+            query = form.cleaned_data['query']
+            context = {}
+            # TODO make this more efficient maybe
+            context['page_results'] = Page.objects.filter(content__icontains=query).values('name', 'title')
+            #context.event_results = Event.objects.filter(description__icontains=query).values('url', 'title') # TODO
+            context['query'] = query
+            return render(request, self.template_name, context)
+        
+        else:
+            return render(request, self.template_name, { page_results: [] })
