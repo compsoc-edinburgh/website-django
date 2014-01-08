7f80918ff9576422e86e9ce5fd78281a930e2ed7
Rikki Guy <rikki.guy@gmail.com>
2014-01-09 00:39:56 +1000

    Event system

--- compsoc/models.py
+++ compsoc/models.py
@@ -9,4 +9,13 @@ class Page(models.Model):
     content = models.TextField()
     
     def __unicode__(self):
-        return self.name
+        return self.title
+
+class Event(models.Model):
+    when = models.DateTimeField()
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    url = models.URLField(blank=True)
+    
+    def __unicode__(self):
+        return self.title
