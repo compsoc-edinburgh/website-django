c24d80d9ed6622e259464622bc209c19b10bf6bd
Rikki Guy <rikki.guy@gmail.com>
2014-01-08 16:24:01 +0000

    More sense

--- compsoc/urls.py
+++ compsoc/urls.py
@@ -3,6 +3,6 @@ from django.views.generic.base import RedirectView
 from compsoc import views
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url='/compsoc/home'), name='go-to-home'),
-    url(r'^(?P<page_name>\w+)', views.PageView.as_view(), name='page'),
+    url(r'^$', RedirectView.as_view(url='/compsoc/page/home'), name='go-to-home'),
+    url(r'^page/(?P<page_name>\w+)', views.PageView.as_view(), name='page'),
 )
