66165ca599e36f82fb2067d4bd2fdf991741ef28
Hamish Hutchings <hamhut1066@gmail.com>
2014-01-09 19:24:42 +0000

    add redirect for root url to homepage

--- compsoc_website_django/urls.py
+++ compsoc_website_django/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
+from compsoc import views
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +10,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'compsoc_website_django.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', RedirectView.as_view(url="/compsoc/page/home"), name="go-to-home"),
     url(r'^compsoc/', include('compsoc.urls')),
+    url(r'^ledger/', include('ledger.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
