df4182712dfc8d5a002bd181e3437951af8efda5
Arkan <arkan@emberwalker.cc>
2014-02-04 19:19:44 +0000

    Revert removal of Events model.
    Reverts a6341dea50b92babd77ce03ef67df1280cdcb35b.

--- compsoc/models.py
+++ compsoc/models.py
@@ -10,3 +10,12 @@ class Page(models.Model):
     
     def __unicode__(self):
         return self.title
+
+class Event(models.Model):
+    when = models.DateTimeField()
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    url = models.URLField(blank=True)
+    
+    def __unicode__(self):
+        return self.title
