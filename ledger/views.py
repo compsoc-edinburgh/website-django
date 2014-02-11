1ca17dfce36da21564bc6da5790a2e9ce01f67c5
Arkan <arkan@emberwalker.cc>
2014-02-11 19:43:33 +0000

    PEP8 cleanup, missed one.

--- ledger/views.py
+++ ledger/views.py
@@ -6,7 +6,7 @@ def home(request):
     # lists ledgers in reverse order
     args = {}
     ledgers = Ledger.objects.all()
-    args['ledgers']= ledgers.order_by('content','id').reverse()
+    args['ledgers'] = ledgers.order_by('content', 'id').reverse()
     args['ledger'] = args['ledgers'].first()
 
     return render_to_response('ledger/ledger_base.html', args)
@@ -15,5 +15,5 @@ def home(request):
 def view_ledger(request, ledger_id=1):
     args = {}
     args['ledger'] = Ledger.objects.get(id=ledger_id)
-    args['ledgers']= Ledger.objects.all()
+    args['ledgers'] = Ledger.objects.all()
     return render_to_response('ledger/ledger.html', args)
