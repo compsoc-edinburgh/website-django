7799abd5746b7512b6872f32c3bfae7198826851
a3gis <henri.maurer@gmail.com>
2014-04-02 15:50:00 +0100

    Add IRC integration through KiwiIRC
    I created a view, IrcView, and a template which wraps around the KiwiIRC
    iframe. We need to add a link to that page in the navigation or on the
    home page. We could also add a header on the IRC page with the compsoc
    logo and a link to go back to the website.

--- compsoc/urls.py
+++ compsoc/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', RedirectView.as_view(url='/compsoc/page/home'), name='go-to-home'),
     url(r'^page/(?P<page_name>\w+)', views.PageView.as_view(), name='compsoc-page'),
     url(r'^search', views.SearchView.as_view(), name='search'),
+    url(r'^irc', views.IrcView.as_view(), name='irc')
 )
